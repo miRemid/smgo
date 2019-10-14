716570d905fbdf1134ea3423bf93992c98502448
miRemid <remidi@126.com>
2019-10-14 14:57:43 +0800

    修改参数

--- login.go
+++ login.go
@@ -9,38 +9,8 @@ import (
 	"github.com/miRemid/smgo/models"
 )
 
-// login 获得用户token
-func (sm *SmClient) login(username, password string) (models.LoginRes, error) {
-	var token models.LoginRes
-	// 1. 构造参数
-	tmp := fmt.Sprintf("username=%s&password=%s", username, password)	
-	params := strings.NewReader(tmp)
-	// 2. 构造请求
-	req, err := http.NewRequest("POST", TokenURL, params)
-	if err != nil {		
-		return token, err
-	}
-	// 添加header
-	req.Header.Add("Content-Type", "application/x-www-form-urlencoded")
-	// 3. 发送请求
-	res, err := sm.HTTPClient.Do(req)
-	if err != nil {		
-		return token, err
-	}
-	// 关闭body
-	defer res.Body.Close()
-	// 4. 解析body到结构体
-	err = json.NewDecoder(res.Body).Decode(&token)
-	if err != nil {		
-		return token, err
-	}
-	// 设置登陆状态	
-	sm.Token = token.Data.Token	
-	return token, nil
-}
-
 // Login 获得用户token
-func (sm *SmClient) Login(username, password string) (models.LoginRes, error){
+func (sm *SmClient) Login(username, password string) (string, error){
 	var token models.LoginRes
 	// 1. 构造参数
 	tmp := fmt.Sprintf("username=%s&password=%s", username, password)	
@@ -48,23 +18,23 @@ func (sm *SmClient) Login(username, password string) (models.LoginRes, error){
 	// 2. 构造请求
 	req, err := http.NewRequest("POST", TokenURL, params)
 	if err != nil {		
-		return token, err
+		return "", err
 	}
 	// 添加header
 	req.Header.Add("Content-Type", "application/x-www-form-urlencoded")
 	// 3. 发送请求
 	res, err := sm.HTTPClient.Do(req)
 	if err != nil {		
-		return token, err
+		return "", err
 	}
 	// 关闭body
 	defer res.Body.Close()
 	// 4. 解析body到结构体
 	err = json.NewDecoder(res.Body).Decode(&token)
 	if err != nil {		
-		return token, err
+		return "", err
 	}
 	// 设置登陆状态	
 	sm.Token = token.Data.Token	
-	return token, nil
+	return token.Data.Token, nil
 }
